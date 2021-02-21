5c3cdd8f9c747a10ea8f3cd407a32e215f7ce77f
Bhushan <bhushan1810patil@gmail.com>
2021-02-22 01:29:01 +0530

    created and added bird.py

---
+++ bird.py
@@ -0,0 +1,17 @@
+import pygame
+import os
+from settings import *
+
+class Bird:
+
+    def __init__(self):
+        self.x = BIRD_X
+        self.y = BIRD_Y
+        self.size = BIRD_SIZE
+        self.color = YELLOW
+
+
+    def draw(self, win: pygame.Surface) -> None:
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.size // 2)
+
+
